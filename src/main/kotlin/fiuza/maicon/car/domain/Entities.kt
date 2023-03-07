62756faf81f50dfc8ef27c06f27cdf1966554c51
MaiconFiuza <maicon.b.fiuza@gmail.com>
2023-03-07 20:27:40 -0300

    :sparkles: adiciona as anotações para a driverAPI

--- src/main/kotlin/fiuza/maicon/car/domain/Entities.kt
+++ src/main/kotlin/fiuza/maicon/car/domain/Entities.kt
@@ -1,10 +1,11 @@
 package fiuza.maicon.car.domain
 
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import java.time.LocalDate
-
+@Entity
 data class Driver(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/kotlin/fiuza/maicon/car/interfaces/DriverApi.kt
+++ src/main/kotlin/fiuza/maicon/car/interfaces/DriverApi.kt
@@ -1,11 +1,24 @@
 package fiuza.maicon.car.interfaces
 
 import fiuza.maicon.car.domain.Driver
+import fiuza.maicon.car.domain.DriverRepository
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @Service
 @RestController
-class DriverApi {
-    fun listDriver() : List<Driver> = emptyList()
+@RequestMapping("/drivers", produces = [MediaType.APPLICATION_JSON_VALUE])
+class DriverApi(
+    val driverRepository: DriverRepository
+) {
+    @GetMapping
+    fun listDriver() : List<Driver> = driverRepository.findAll();
+
+    @GetMapping("{id}")
+    fun listDriverById(@PathVariable id: Long) : Driver = driverRepository.findById(id).orElseThrow()
+
 }
