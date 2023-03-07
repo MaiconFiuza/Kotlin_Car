53790eb7bb482cf7a691ec0a27b943059d7e70ed
MaiconFiuza <maicon.b.fiuza@gmail.com>
2023-03-06 22:22:09 -0300

    :tada: configurações iniciais do projeto

---
+++ src/test/kotlin/fiuza/maicon/car/CarApplicationTests.kt
@@ -0,0 +1,13 @@
+package fiuza.maicon.car
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
